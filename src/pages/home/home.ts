74e1873621d69e6625e754e876f6e7fed50a5cbc
Shilo White <shilo86@gmail.com>
2016-10-07 06:00:39 -0700

    Added camera test.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 import {About} from '../about/about';
+import {Camera} from 'ionic-native';
 
 @Component({
   selector: 'page-home',
@@ -9,9 +10,23 @@ import {About} from '../about/about';
 })
 export class HomePage {
   aboutPage = About;
+  base64Image: string;
 
   constructor(public navCtrl: NavController) {
     
+  }
+
+  takePicture() {
+    Camera.getPicture({
+        destinationType: Camera.DestinationType.DATA_URL,
+        targetWidth: 1000,
+        targetHeight: 1000
+    }).then((imageData) => {
+      // imageData is a base64 encoded string
+        this.base64Image = "data:image/jpeg;base64," + imageData;
+    }, (err) => {
+        console.log(err);
+    });
   }
 
 }
