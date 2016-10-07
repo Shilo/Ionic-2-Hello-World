f52cdd2fe57cc0f0c226011077aef357baeb5727
Shilo White <shilo86@gmail.com>
2016-10-07 05:42:45 -0700

    Added sample about page.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import {About} from '../about/about';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  aboutPage = About;
 
   constructor(public navCtrl: NavController) {
     
